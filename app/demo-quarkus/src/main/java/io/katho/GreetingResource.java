4f7c6dd1187d8f9dd7324972ead6509fd9af0436
paulokatho <pkatho@gmail.com>
2024-04-29 10:50:48 +0100

    refactor: refactor de novo arquivo controller

--- app/demo-quarkus/src/main/java/io/katho/GreetingResource.java
+++ app/demo-quarkus/src/main/java/io/katho/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello Katho New Action Reactive And Atomic";
+        return "Hello Katho New Action Reactive And Atomic E AGORA ENTRANDO PARA O DIGITAL!!!!";
     }
 }
