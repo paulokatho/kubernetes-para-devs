24a30a4a42a9d54cd85bdd2f0adf91c92e39d998
paulokatho <pkatho@gmail.com>
2024-06-06 08:38:20 +0100

    refactor: IA e IA Generativa

--- app/demo-quarkus/src/main/java/io/katho/GreetingResource.java
+++ app/demo-quarkus/src/main/java/io/katho/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello Katho New Action Reactive And Atomic! Vamos.";
+        return "Hello Katho New Action Reactive And Atomic! Hoje foi aula de inteligencia artificial e generativa.";
     }
 }
